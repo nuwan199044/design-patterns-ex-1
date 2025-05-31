efa23613584735e79bd019de9b62773e30b60938
Nuwan <nuwan199044@gmail.com>
2025-05-31 18:07:59 +0530

    initial code

---
+++ src/test/java/com/myapp/design_pattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.design_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
