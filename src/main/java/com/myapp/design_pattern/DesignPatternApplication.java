a6bcd74d563d58cb5ccd08f494223ba4e5763f04
Nuwan <nuwan199044@gmail.com>
2025-05-31 21:13:14 +0530

    Revert "singleton pattern implementation"
    This reverts commit 1b5b190437c187cf9a9aa9a84ac44152bda4eadd.

--- src/main/java/com/myapp/design_pattern/DesignPatternApplication.java
+++ src/main/java/com/myapp/design_pattern/DesignPatternApplication.java
@@ -1,34 +1,13 @@
 package com.myapp.design_pattern;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
-public class DesignPatternApplication implements CommandLineRunner {
+public class DesignPatternApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesignPatternApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Singleton instance1 = Singleton.getInstance("Hello");
-		log.info("data from instance 1: {}", instance1.getData());
-
-		Singleton instance2 = Singleton.getInstance("Hi");
-		log.info("data from instance 2: {}", instance2.getData());
-
-		AppConfig config = AppConfig.INSTANCE;
-		config.printConfig();
-
-		AppConfig anotherConfig = AppConfig.INSTANCE;
-		anotherConfig.printConfig();
-
-		System.out.println(anotherConfig.getConfigName());
-
 	}
 
 }
